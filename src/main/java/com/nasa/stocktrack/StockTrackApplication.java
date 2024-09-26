40d72a2179304f62d2745a920c6e115359392c9a
nivaldoandrade <nivaldoandradef@gmail.com>
2024-09-26 13:12:02 -0300

    chore: initialize project and configure postgresql
    - Initialized project structure
    - Configured postgresql settings in application.properties

---
+++ src/main/java/com/nasa/stocktrack/StockTrackApplication.java
@@ -0,0 +1,13 @@
+package com.nasa.stocktrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nasa/stocktrack/StockTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nasa.stocktrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
