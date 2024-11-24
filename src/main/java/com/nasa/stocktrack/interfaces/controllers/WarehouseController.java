51d3143e52c334408ad8c5027f323ced2f7af327
nivaldoandrade <nivaldoandradef@gmail.com>
2024-11-24 13:54:30 -0300

    refactor: add OpenAPI annotations for WarehouseController
    - Created WarehouseControllerOpenAPI interface with OpenAPI annotations for better API documentation
    - Updated WarehouseController to implement WarehouseControllerOpenAPI enhancing documentation for endpoints
    - Added operation summary, description and response examples for endpoints
    - Defined supported media types for endpoints

--- src/main/java/com/nasa/stocktrack/interfaces/controllers/WarehouseController.java
+++ src/main/java/com/nasa/stocktrack/interfaces/controllers/WarehouseController.java
@@ -11,9 +11,11 @@ import com.nasa.stocktrack.interfaces.dtos.ListResponseDTO;
 import com.nasa.stocktrack.interfaces.dtos.warehouse.CreateWarehouseDTO;
 import com.nasa.stocktrack.interfaces.dtos.warehouse.UpdateWarehouseRequestDTO;
 import com.nasa.stocktrack.interfaces.dtos.warehouse.WarehouseDTO;
+import com.nasa.stocktrack.interfaces.openapi.controllers.WarehouseControllerOpenAPI;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
@@ -25,7 +27,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/warehouses")
 @AllArgsConstructor
-public class WarehouseController {
+public class WarehouseController implements WarehouseControllerOpenAPI {
 
     private final CreateWarehouseUseCase createWarehouseUseCase;
     private final ShowWarehouseUseCase showWarehouseUseCase;
@@ -33,7 +35,8 @@ public class WarehouseController {
     private final UpdateWarehouseUseCase updateWarehouseUseCase;
     private final DeleteWarehouseUseCase deleteWarehouseUseCase;
 
-    @GetMapping
+    @Override
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ListResponseDTO<WarehouseDTO>> list(
             @RequestParam(name = "page", defaultValue = "0") @Min(0) Integer page,
             @RequestParam(name = "size", defaultValue = "10") @Min(1) @Max(10) Integer size,
@@ -50,15 +53,17 @@ public class WarehouseController {
         return ResponseEntity.ok(ListResponseDTO.toResponse(warehousePaginatedList, WarehouseDTO::toResponse));
     }
 
-    @GetMapping("/{id}")
+    @Override
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<WarehouseDTO> show(@ValidUUID @PathVariable String id) {
         Warehouse warehouse = showWarehouseUseCase.execute(UUID.fromString(id));
 
         return ResponseEntity.ok(WarehouseDTO.toResponse(warehouse));
     }
 
+    @Override
     @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<WarehouseDTO> create(@RequestBody @Validated CreateWarehouseDTO createWarehouseDTO) {
 
         Warehouse warehouse = createWarehouseUseCase.execute(CreateWarehouseDTO.toDomain(createWarehouseDTO));
@@ -68,8 +73,9 @@ public class WarehouseController {
         return ResponseEntity.created(uri).body(WarehouseDTO.toResponse(warehouse));
     }
 
+    @Override
     @PreAuthorize("hasRole('ADMIN')")
-    @PutMapping("/{id}")
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> update(
             @ValidUUID @PathVariable String id,
             @RequestBody @Validated UpdateWarehouseRequestDTO updateWarehouseRequestDTO
@@ -81,8 +87,9 @@ public class WarehouseController {
         return ResponseEntity.noContent().build();
     }
 
+    @Override
     @PreAuthorize("hasRole('ADMIN')")
-    @DeleteMapping("/{id}")
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> delete(@ValidUUID @PathVariable String id) {
 
         deleteWarehouseUseCase.execute(UUID.fromString(id));
---
+++ src/main/java/com/nasa/stocktrack/interfaces/openapi/controllers/WarehouseControllerOpenAPI.java
@@ -0,0 +1,123 @@
+package com.nasa.stocktrack.interfaces.openapi.controllers;
+
+import com.nasa.stocktrack.infra.constraints.EnumOrderByPattern;
+import com.nasa.stocktrack.infra.constraints.ValidUUID;
+import com.nasa.stocktrack.infra.exceptions.RestErrorResponseWithErrors;
+import com.nasa.stocktrack.interfaces.dtos.ListResponseDTO;
+import com.nasa.stocktrack.interfaces.dtos.warehouse.CreateWarehouseDTO;
+import com.nasa.stocktrack.interfaces.dtos.warehouse.UpdateWarehouseRequestDTO;
+import com.nasa.stocktrack.interfaces.dtos.warehouse.WarehouseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Tag(name = "Warehouse", description = "Warehouse management API")
+public interface WarehouseControllerOpenAPI {
+
+    @Operation(
+            summary = "Retrieve all Warehouses",
+            description = "Get a Warehouses array",
+            parameters = {
+                    @Parameter(
+                            name = "page",
+                            description = "Page number for pagination. Min value: 0",
+                            example = "0"
+                    ),
+                    @Parameter(
+                            name = "size",
+                            description = "Number of items per page. Min value: 1 - Max value: 10",
+                            example = "10"
+                    ),
+                    @Parameter(
+                            name = "orderBy",
+                            description = "Sort by ascendant or descendant",
+                            schema = @Schema(
+                                    types = "String",
+                                    allowableValues = {"ASC", "DESC"},
+                                    defaultValue = "ASC"
+                            )
+                    ),
+            }
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", content = @Content),
+            @ApiResponse(responseCode = "403", content = @Content)
+    })
+    ResponseEntity<ListResponseDTO<WarehouseDTO>> list(
+            @RequestParam(name = "page", defaultValue = "0") @Min(0) Integer page,
+            @RequestParam(name = "size", defaultValue = "10") @Min(1) @Max(10) Integer size,
+            @RequestParam(name = "orderBy", defaultValue = "asc") @EnumOrderByPattern String orderBy,
+            @RequestParam(required = false) String search
+    );
+
+    @Operation(
+            summary = "Retrieve a Warehouse by id",
+            description = "Get a Warehouse by id. The response is object of the WarehouseDTO schema type"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", content = @Content),
+            @ApiResponse(responseCode = "403", content = @Content),
+            @ApiResponse(responseCode = "404", content = @Content)
+    })
+    ResponseEntity<WarehouseDTO> show(@ValidUUID @PathVariable String id);
+
+    @Operation(
+            summary = "Create a Warehouse",
+            description = "Create a new Warehouse by passing in a JSON representation of the CreateWarehouseDTO schema type"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", content = {@Content(
+                    schema = @Schema(implementation = RestErrorResponseWithErrors.class),
+                    mediaType = MediaType.APPLICATION_JSON_VALUE
+            )}),
+            @ApiResponse(responseCode = "401", content = @Content),
+            @ApiResponse(responseCode = "403", content = @Content),
+            @ApiResponse(responseCode = "404", content = @Content)
+    })
+    ResponseEntity<WarehouseDTO> create(@RequestBody @Validated CreateWarehouseDTO createWarehouseDTO);
+
+    @Operation(
+            summary = "Update a Warehouse by id",
+            description = "Update a Warehouse by id. passing in a JSON representation of the UpdateWarehouseRequestDTO schema type"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "204"),
+            @ApiResponse(responseCode = "400", content = {@Content(
+                    schema = @Schema(implementation = RestErrorResponseWithErrors.class),
+                    mediaType = MediaType.APPLICATION_JSON_VALUE
+            )}),
+            @ApiResponse(responseCode = "401", content = @Content),
+            @ApiResponse(responseCode = "403", content = @Content),
+            @ApiResponse(responseCode = "404", content = @Content),
+    })
+    ResponseEntity<Void> update(
+            @ValidUUID @PathVariable String id,
+            @RequestBody @Validated UpdateWarehouseRequestDTO updateWarehouseRequestDTO
+    );
+
+    @Operation(
+            summary = "Delete a Warehouse by id",
+            description = "Delete a Warehouse by id"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "204"),
+            @ApiResponse(responseCode = "400", content = @Content),
+            @ApiResponse(responseCode = "404", content = @Content)
+    })
+    ResponseEntity<Void> delete(@ValidUUID @PathVariable String id);
+}
