f7a6439a1cb4cc5a2dcf8483f89217a7dd00fd8d
nivaldoandrade <nivaldoandradef@gmail.com>
2024-11-24 14:03:36 -0300

    refactor: Normalize API documentation and response media type for AuthController
    - Updated AuthController to explicitly set produces = MediaType.APPLICATION_JSON_VALUE for endpoints
    - Simplified AuthControllerOpenAPI by removing redundant annotations and schema definitions for endpoints, as Swagger generates this automatically based on the return type

--- src/main/java/com/nasa/stocktrack/interfaces/controllers/AuthController.java
+++ src/main/java/com/nasa/stocktrack/interfaces/controllers/AuthController.java
@@ -5,6 +5,7 @@ import com.nasa.stocktrack.interfaces.dtos.auth.SignInRequestDTO;
 import com.nasa.stocktrack.interfaces.dtos.auth.TokenResponseDTO;
 import com.nasa.stocktrack.interfaces.openapi.controllers.AuthControllerOpenAPI;
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +21,7 @@ public class AuthController implements AuthControllerOpenAPI {
     private final SignInUseCase signInUseCase;
 
     @Override
-    @PostMapping("/signin")
+    @PostMapping(value = "/signin", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<TokenResponseDTO> signIn(@Validated @RequestBody SignInRequestDTO signInRequestDTO) {
 
         String accessToken = signInUseCase.execute(signInRequestDTO.toDomain());
--- src/main/java/com/nasa/stocktrack/interfaces/openapi/controllers/AuthControllerOpenAPI.java
+++ src/main/java/com/nasa/stocktrack/interfaces/openapi/controllers/AuthControllerOpenAPI.java
@@ -4,18 +4,14 @@ import com.nasa.stocktrack.interfaces.dtos.auth.SignInRequestDTO;
 import com.nasa.stocktrack.interfaces.dtos.auth.TokenResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Auth", description = "Auth management API")
-@RestController
 public interface AuthControllerOpenAPI {
 
     @Operation(
@@ -23,10 +19,7 @@ public interface AuthControllerOpenAPI {
             description = "Authenticate user with provided credentials and generate authentication token."
     )
     @ApiResponses({
-            @ApiResponse(responseCode = "200", content = {@Content(
-                    schema = @Schema(implementation = TokenResponseDTO.class),
-                    mediaType = MediaType.APPLICATION_JSON_VALUE
-            )}),
+            @ApiResponse(responseCode = "200"),
             @ApiResponse(responseCode = "400", content = @Content)
     })
     ResponseEntity<TokenResponseDTO> signIn(@Validated @RequestBody SignInRequestDTO signInRequestDTO);
