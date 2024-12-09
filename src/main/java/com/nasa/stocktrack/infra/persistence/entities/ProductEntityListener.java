c0c7b2b02bc239dd9ad0e17cb6477993b49f3d9d
nivaldoandrade <nivaldoandradef@gmail.com>
2024-12-09 14:29:41 -0300

    refactor: enhance ProductEntityListener with dynamic image URL resolution
    - Refactored ProductEntityListener to dynamically generate image URLs based on the storage type(LOCAL or S3)
    - Added getUrlLocal method for local file storage URL generation
    - Added getUrlS3 method to generate URL for image stored in S3

--- src/main/java/com/nasa/stocktrack/infra/persistence/entities/ProductEntityListener.java
+++ src/main/java/com/nasa/stocktrack/infra/persistence/entities/ProductEntityListener.java
@@ -1,10 +1,25 @@
 package com.nasa.stocktrack.infra.persistence.entities;
 
+import com.nasa.stocktrack.infra.config.storage.FileStorageProperties;
 import jakarta.persistence.PostLoad;
 import jakarta.persistence.PrePersist;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetUrlRequest;
+
 
 public class ProductEntityListener {
+    private final FileStorageProperties fileStorageProperties;
+
+    @Autowired(required = false)
+    private S3Client s3Client;
+
+    public ProductEntityListener(FileStorageProperties fileStorageProperties) {
+        this.fileStorageProperties = fileStorageProperties;
+    }
 
     @PostLoad
     @PrePersist
@@ -13,14 +28,32 @@ public class ProductEntityListener {
             return;
         }
 
-        String imageUrl = ServletUriComponentsBuilder
-                .fromCurrentContextPath()
-                .path("products/images/")
-                .path(productEntity.getImage())
-                .toUriString();
+        String imageName = productEntity.getImage();
 
-
+        String imageUrl = switch (fileStorageProperties.getType()) {
+            case S3 -> getUrlS3(imageName);
+            case LOCAL -> getUrlLocal(imageName);
+        };
         productEntity.setImageUrl(imageUrl);
     }
 
+    private String getUrlLocal(String image) {
+        return ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .path("products/images/")
+                .path(image)
+                .toUriString();
+    }
+
+    private String getUrlS3(String image) {
+        String bucketName = fileStorageProperties.getS3().getBucketName();
+
+        GetUrlRequest getUrlRequest = GetUrlRequest.builder()
+                .bucket(bucketName)
+                .key(image)
+                .build();
+
+
+        return s3Client.utilities().getUrl(getUrlRequest).toString();
+    }
 }
